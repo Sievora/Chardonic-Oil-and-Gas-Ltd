4df90f44e380bf6412ca354bf43191b0cd9d85f9
Your Name <you@example.com>
2026-04-29 11:39:20 +0100

    11:39:52 4/29/2026

---
+++ script.js
@@ -0,0 +1,10 @@
+{
+    const navBar = document.getElementById('navBar');
+    function closeNav() {
+        navBar.style.display = 'none';
+    }
+
+     function openNav() {
+        navBar.style.display = 'grid';
+    }
+}
