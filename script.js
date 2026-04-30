f1f9ca72ad4c76566a69153df5a906ec0bdc46d2
Your Name <you@example.com>
2026-04-30 16:10:39 +0100

    anime

--- script.js
+++ script.js
@@ -9,30 +9,3 @@
     }
 }
 
-{
-
-const counter = document.getElementById("counter");
-const duration = 2000; // 2 seconds
-const end = 1000;
-
-let startTime = null;
-
-function animateCounter(timestamp) {
-  if (!startTime) startTime = timestamp;
-  const progress = timestamp - startTime;
-
-  const value = Math.min(
-    Math.floor((progress / duration) * end),
-    end
-  );
-
-  counter.innerText = value;
-
-  if (progress < duration) {
-    requestAnimationFrame(animateCounter);
-  }
-}
-
-requestAnimationFrame(animateCounter);
-
-}
