1dea7ea054d1e923c2141d3b6d3970ccfd0a7f4f
Your Name <you@example.com>
2026-05-04 17:02:29 +0100

    last last one

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 {
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const target = document.querySelector(targetId);
+
+      if (!target) return;
+
+      const start = window.pageYOffset;
+      const end = target.getBoundingClientRect().top + start;
+      const duration = 1000; // 3 second
+
+      let startTime = null;
+
+      function scrollAnimation(currentTime) {
+        if (!startTime) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+
+        const progress = Math.min(timeElapsed / duration, 1);
+        window.scrollTo(0, start + (end - start) * progress);
+
+        if (timeElapsed < duration) {
+          requestAnimationFrame(scrollAnimation);
+        }
+      }
+
+      requestAnimationFrame(scrollAnimation);
+    });
+  });
+
+}
+
+
+
+{
     const navBar = document.getElementById('navBar');
     function closeNav() {
         navBar.style.display = 'none';
