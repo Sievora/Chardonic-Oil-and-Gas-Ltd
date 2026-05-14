6e5db4f43061e5a8ed1e9c81cd96bfa1c9482dd5
Your Name <you@example.com>
2026-05-14 14:21:22 +0100

    animation1

--- script.js
+++ script.js
@@ -56,3 +56,38 @@ function closeget() {
 }
 
 }
+
+
+{
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else { 
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+}
+
+{
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('showTwo');
+      } else { 
+        entry.target.classList.remove('showTwo');
+      }
+    });
+  });
+
+  const hiddenTwoElements = document.querySelectorAll('.hiddenTwo');
+  hiddenTwoElements.forEach((el) => observer.observe(el));
+}
